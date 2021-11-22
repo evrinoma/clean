407c2ef78f453bde0e04c7098baf59a6dee6e0b4
Nikolay Nikolaev <evrinoma@gmail.com>
2021-11-22 20:50:26 +0300

    fix abstract layer test case

--- tests/Functional/CaseTest.php
+++ tests/Functional/CaseTest.php
@@ -5,22 +5,29 @@ namespace App\Tests\Functional;
 use App\Tests\Functional\Helper\AuthorizationTrait;
 use Evrinoma\TestUtilsBundle\Browser\ApiBrowserTestInterface;
 use Evrinoma\TestUtilsBundle\Browser\ApiBrowserTestTrait;
-use Evrinoma\TestUtilsBundle\Helper\ApiHelperTestInterface;
-use Evrinoma\TestUtilsBundle\Helper\ApiHelperTestTrait;
+use Evrinoma\TestUtilsBundle\Helper\AbstractSymfony;
+use Evrinoma\TestUtilsBundle\Helper\ApiMethodTestInterface;
+use Evrinoma\TestUtilsBundle\Helper\ApiMethodTestTrait;
+use Evrinoma\TestUtilsBundle\Helper\DoctrineTestTrait;
+use Evrinoma\TestUtilsBundle\Helper\ResponseStatusTestTrait;
 use Evrinoma\TestUtilsBundle\Web\AbstractWebCaseTest;
 
-abstract class CaseTest extends AbstractWebCaseTest implements ApiBrowserTestInterface, ApiHelperTestInterface
+abstract class CaseTest extends AbstractWebCaseTest  implements ApiBrowserTestInterface, ApiMethodTestInterface
 {
-    use ApiHelperTestTrait, ApiBrowserTestTrait, AuthorizationTrait;
+    use ApiMethodTestTrait, ApiBrowserTestTrait, AuthorizationTrait, DoctrineTestTrait, ResponseStatusTestTrait;
 
 //region SECTION: Protected
     protected function setUp(): void
     {
-        static::$default = static::defaultData();
+        self::bootKernel();
 
-        $this->setUrl();
+        parent::setUp();
 
-        $this->client = $this->getAuthorizationClient();
+        $container = AbstractSymfony::checkVersion() ? $this->getContainer() : static::$container;
+
+        $this->setEntityManager($container);
+
+        $this->getAuthorization($container, $this->client);
     }
 
     protected function tearDown(): void
